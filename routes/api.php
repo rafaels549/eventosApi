d61585b787354949878e7abc3b3a62265b16f568
rafaels549 <rafalesantoscostat@gmail.com>
2025-02-15 16:32:11 -0300

    first commit

---
+++ routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function (Request $request) {
+
+ return response()->json(['message' => 'Hello World!']);
+});
